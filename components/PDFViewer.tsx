84b89e6f938a61408102a061231b99936d9481ff
Adhurim Gervalla <adi@hausformat.com>
2023-11-03 13:24:32 +0100

    optimizes pdf output

--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -1,7 +1,12 @@
 'use client';
-import React, { useEffect, useState } from 'react'
+import { is } from 'drizzle-orm';
+import { Loader2 } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react'
+import { set } from 'react-hook-form';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -14,23 +19,28 @@ type Props = {
 
 const PDFViewer = ({pdf_url}: Props) => {
   const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [pageWidth, setPageWidth] = useState(500);
+  const [pageDimensions, setPageDimensions] = useState({ width: 0, height: 0 });
+  const [loading, setLoading] = useState(true);
+  const pdfContainerRef = useRef<HTMLDivElement>(null);
+
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
+    setLoading(false);
   }
 
   useEffect(() => {
-    // Function to update the width
     const updatePageWidth = () => {
-      setPageWidth(window.innerWidth); // Or the width of a specific container if necessary
+      if (pdfContainerRef && pdfContainerRef.current) {
+        setPageDimensions({
+          width: pdfContainerRef.current.clientWidth,
+          height: pdfContainerRef.current.clientHeight,
+        });
+      }
     };
-
     // Update page width on initial mount and when window resizes
     window.addEventListener('resize', updatePageWidth);
     updatePageWidth();
 
-    // Cleanup
     return () => window.removeEventListener('resize', updatePageWidth);
   }, []);
 
@@ -38,17 +48,18 @@ const PDFViewer = ({pdf_url}: Props) => {
     const pages = [];
     for (let i = 1; i <= numPages!; i++) {
       pages.push(
-        <Page key={i} pageNumber={i} width={1000} />
+        <Page key={i} pageNumber={i} width={pageDimensions.width} />
       );
     }
     return pages;
   };
 
   return (
-    <div>
-          <Document file={pdf_url} onLoadSuccess={onDocumentLoadSuccess}>
+    <div id="pdf-viewer-container" ref={pdfContainerRef}>
+          <Document file={pdf_url} onLoad={() => setLoading(true)} onLoadSuccess={onDocumentLoadSuccess}>
             {numPages && renderPages()}
           </Document>
+          {loading && <Loader2 className='w-4 h-4 animate-spin inline' />}
           <p>
       </p>  
     </div>
