6310a9c0bb3c918d15a544e9ad99b8bab1920571
Adhurim Gervalla <adi@hausformat.com>
2023-11-05 20:30:47 +0100

    removes cringe background color

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
   }
 
   return (
-    <div className='w-screen min-h-screen bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'>
+    <div className='w-screen min-h-screen'>
       <div className='left-1/2 -translate-x-1/2 -translate-y-1/2 absolute top-1/2 max-w-screen-xl pt-10'>
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
@@ -35,9 +35,9 @@ export default async function Home() {
             {isAuth && firstChat && <Link href={`/chats/${firstChat.id}`}><Button>Go to chats <ArrowRight className='ml-2' /></Button></Link>}
             {isAuth && <div className='ml-3'><SubscriptionButton isPro={isPro}/></div>}
           </div>
-          {!userId && <p className='text-lg mt-5'>Join now to unlock your full potential</p>}
+          {!userId && <p className='mt-3'>Join now to unlock your full potential</p>}
           <div className='mt-4 max-w-sm w-full'>
-            {isAuth ? (<FileUpload />) : <Link href={'/sign-in'}><Button>Login to get started <LogIn className='w-5 h-5 ml-2' /></Button></Link>}
+            {isAuth ? (<FileUpload />) : <Link href={'/sign-in'}><Button>Login<LogIn className='w-5 h-5 ml-2' /></Button></Link>}
           </div>
         </div>
       </div>
