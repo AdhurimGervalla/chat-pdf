70659f0391e34010e192a7080301e717ece067b3
Adhurim Gervalla <adi@hausformat.com>
2023-11-07 07:23:44 +0100

    fix messages

--- app/api/get-messages/route.ts
+++ app/api/get-messages/route.ts
@@ -1,13 +1,13 @@
 import { db } from "@/lib/db";
 import { messages } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
 export const POST = async (req: Request) => {
     const {chatId} = await req.json();
-    const _messages = await db.select().from(messages).where(eq(messages.chatId, chatId));
+    const _messages = (await db.select().from(messages).orderBy((asc(messages.createdAt))).where(eq(messages.chatId, chatId)));
     const convertedMessages = _messages.map(msg => {
         let pageNumbers;
         if (msg.pageNumbers) {
