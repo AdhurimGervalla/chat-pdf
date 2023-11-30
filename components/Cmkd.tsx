ae767e288196805282e5dcb3fcf4c1ac481cd521
Adhurim Gervalla <adhurim@gervalla.ch>
2023-11-30 23:40:10 +0100

    adds workspaces functionalities to cmdk

--- components/Cmkd.tsx
+++ components/Cmkd.tsx
@@ -5,11 +5,13 @@ import { DrizzleChat, DrizzleWorkspace } from '@/lib/db/schema';
 import Link from 'next/link';
 import { v4 } from "uuid";
 import { useRouter, useParams } from 'next/navigation';
-import { ArrowRight, Bookmark, LinkIcon, PlusCircle, Search, Trash } from 'lucide-react';
+import { ArrowRight, Bookmark, LinkIcon, PlusCircle, Save, Search, Trash } from 'lucide-react';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import { cn } from '@/lib/utils';
 import { ChatBubbleLeftIcon } from '@heroicons/react/20/solid';
+import { set } from 'react-hook-form';
+import { Button } from './ui/button';
 
 type Props = {
     chats: DrizzleChat[];
@@ -19,20 +21,25 @@ type Props = {
 const Cmkd = ({chats, workspaces}: Props) => {
     const router = useRouter();
     const { chatId } = useParams();
-    const inputRef = React.useRef<HTMLInputElement>(null)
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const workspaceNameInputRef = React.useRef<HTMLInputElement>(null);
     const [open, setOpen] = React.useState(false);
     const [search, setSearch] = React.useState('')        
     const [pages, setPages] = React.useState<string[]>([])
     const [selectedChat, setSelectedChat] = React.useState<DrizzleChat | null>(null)
+    const [saveToWorkspaceMode, setSaveToWorkspaceMode] = React.useState(false)
+    const [workspaceName, setWorkspaceName] = React.useState<string>('');
+    const [disableDialogInput, setDisableDialogInput] = React.useState<boolean>(false);
     const page = pages[pages.length - 1]
 
     // Toggle the menu when ⌘K is pressed
     React.useEffect(() => {
         const down = (e: any) => {
             if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+                console.log('pressed');
                 e.preventDefault();
-                setSelectedChat(null);
-                setOpen((open) => !open)
+                setOpen((open) => !open);
+                reset();
             }
         }
 
@@ -40,10 +47,26 @@ const Cmkd = ({chats, workspaces}: Props) => {
         return () => document.removeEventListener('keydown', down)
     }, []);
 
+    const reset = () => {
+        setPages([]);
+        setSearch('');
+        setWorkspaceName('');
+        setSelectedChat(null);
+        setSaveToWorkspaceMode(false);
+        setDisableDialogInput(false);
+    }
+
     const CommandItemClasses = 'mt-3 first:mt-0 cursor-pointer flex flex-row items-center gap-2 bg-slate-700 py-1 px-3 rounded-lg hover:bg-slate-800 transition-colors';
 
     const handleValueChange = (value: string) => {
         setSearch(value);
+    }
+
+    const handleWorkspaceSelect = () => {
+        setSaveToWorkspaceMode(!saveToWorkspaceMode);
+        if (inputRef.current) {
+            inputRef.current.focus();
+          }
     }
 
     const handeOnSelect = (page: string) => {
@@ -51,6 +74,16 @@ const Cmkd = ({chats, workspaces}: Props) => {
         if (inputRef.current) {
             inputRef.current.focus();
           }
+    }
+
+    const handleCreateWorkspace = (page: string) => {
+        setDisableDialogInput(!disableDialogInput);
+        setPages([...pages, page]);
+        setTimeout(() => {
+            if (workspaceNameInputRef.current) {
+                workspaceNameInputRef.current.focus();
+            }
+        }, 100);
     }
 
     const handleDetailView = (chat: DrizzleChat) => {
@@ -116,21 +149,74 @@ const Cmkd = ({chats, workspaces}: Props) => {
         return id === chatId;
     }
 
+
+    const handleSaveToWorkspace = async (workspace: DrizzleWorkspace) => {
+        if (workspace && selectedChat) {
+          try {
+            toast.promise(axios.post(`/api/save-chat-to-workspace`, {
+                workspace,
+                chat: selectedChat
+              }), {
+                loading: 'Saving chat to workspace...',
+                success: 'Chat saved to workspace',
+                error: 'Couldn\'t save chat to workspace'
+            }).then(() => {
+                console.log('refreshing');
+                router.refresh();
+                setSaveToWorkspaceMode(false);
+            });
+          } catch (error: any) {
+              // check if error status is 409
+              // if so, then set workspaceName to ''
+              // else, console.log(error)
+              if (error.response.status === 409) {
+                toast.error('Chat already exists in this workspace');
+              }
+          }
+        }
+    }
+
+    const createWorkspace = async () => {
+        if (workspaceName.length > 0) {
+          try {
+            toast.promise(axios.post(`/api/create-workspace`, {
+                workspaceName
+              }), {
+                loading: 'Creating new workspace...',
+                success: 'Saved workspace',
+                error: 'Couldn\'t save workspace'
+            }).then(() => {
+                console.log('refreshing');
+                router.refresh();
+                reset();
+            });
+          } catch (error: any) {
+              // check if error status is 409
+              // if so, then set workspaceName to ''
+              // else, console.log(error)
+              if (error.response.status === 409) {
+                toast.error('Chat already exists in this workspace');
+              }
+          }
+        }
+    }
+    
+    
     return (
         <>
-        {false && open && <div className='blur-xl fixed left-0 right-0 top-0 bottom-0 bg-black opacity-70 z-10'>
+        {open && <div className='blur-xl fixed left-0 right-0 top-0 bottom-0 bg-black opacity-70 z-10'>
 
         </div>}
         <Command.Dialog onKeyDown={(e) => {
             // Escape goes to previous page
             // Backspace goes to previous page when search is empty
-            if (e.key === 'Escape' || (e.key === 'Backspace' && !search)) {
+            if (e.key === 'Escape' || (e.key === 'Backspace' && !search && !disableDialogInput)) {
+                console.log('pressed');
               e.preventDefault();
-              setSelectedChat(null);
-              setPages((pages) => pages.slice(0, -1));
+              reset();
             }
-          }} className='shadow-3xl fixed z-20 top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] max-h-[500px] h-full overflow-y-scroll bg-slate-900 rounded-lg opacity-[0.98]' open={open} onOpenChange={setOpen} label="Global Command Menu">
-            <Command.Input placeholder='search for chats and workspaces' ref={inputRef} value={search} onValueChange={handleValueChange} className='px-5 py-3 sticky top-0 w-full text-slate-900 bg-slate-900 opacity-100 border-0 focus:ring-0 focus:ring-offset-0 dark:text-white' />
+          }} className='shadow-3xl fixed z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] max-h-[500px] h-full overflow-y-scroll bg-slate-900 rounded-lg opacity-[0.98]' open={open} onOpenChange={setOpen} label="Global Command Menu">
+            <Command.Input disabled={disableDialogInput} placeholder={!disableDialogInput ? 'search for chats and workspaces' : ''} ref={inputRef} value={search} onValueChange={handleValueChange} className='px-5 py-3 sticky top-0 w-full text-slate-900 bg-slate-900 opacity-100 border-0 focus:ring-0 focus:ring-offset-0 dark:text-white' />
             <Command.List className='p-5'>
                 {!page && (
                     <>
@@ -148,6 +234,7 @@ const Cmkd = ({chats, workspaces}: Props) => {
                         <Command.Group>
                                 <small>Workspaces</small>
                                 <Command.Item className={CommandItemClasses} onSelect={() => handeOnSelect('workspaces')}><Search className='w-4 h-4' />Search workspaces</Command.Item>
+                                <Command.Item className={CommandItemClasses} onSelect={() => handleCreateWorkspace('newWorkspace')}><PlusCircle className='w-4 h-4' />New Workspace</Command.Item>
                         </Command.Group>
                     </>
                 )}
@@ -166,12 +253,12 @@ const Cmkd = ({chats, workspaces}: Props) => {
                 {page && selectedChat && (
                     <>
                         <h3 className='text-lg mb-5'>{selectedChat.title && trimChatTitle(selectedChat.title)}</h3>
-                        <Command.Item className={cn(CommandItemClasses, {'opacity-50 cursor-default hover:bg-slate-700': isCurrentChat(selectedChat?.id)})} onSelect={() => {
+                        {!saveToWorkspaceMode && <><Command.Item className={cn(CommandItemClasses, {'opacity-50 cursor-default hover:bg-slate-700': isCurrentChat(selectedChat?.id)})} onSelect={() => {
                             if (!isCurrentChat(selectedChat.id)) {
                                 router.push(`/chats/${selectedChat.id}`);
                             }
                         }}>
-                            <ChatBubbleLeftIcon className='w-4 h-4' /> Open chat
+                        <ChatBubbleLeftIcon className='w-4 h-4' /> Open chat
                         </Command.Item>
                         <Command.Item className={cn(CommandItemClasses, {'bg-slate-800': selectedChat.bookmarked})} onSelect={() => toggleBookmark(selectedChat)}>
                             <Bookmark className='w-4 h-4' color={selectedChat.bookmarked ? 'green' : 'white'} /> Bookmark Chat
@@ -181,13 +268,26 @@ const Cmkd = ({chats, workspaces}: Props) => {
                         </Command.Item>
                         <Command.Item className={CommandItemClasses}>
                             <LinkIcon className='w-4 h-4' /> Show related chats
-                        </Command.Item>
-                        <div className='mt-5'>
+                        </Command.Item></>}
+                        {selectedChat.workspaceId === 0 && <Command.Item onSelect={handleWorkspaceSelect} className={CommandItemClasses}>
+                            <PlusCircle className='w-4 h-4' /> Add to workspace
+                        </Command.Item>}
+                        <Command.Group className='mt-5'>
                             <span>Workspace:</span>
-                        {workspaces.map((workspace) => (
+                            {saveToWorkspaceMode && (
+                                <>
+                                    {workspaces.map((workspace) => (
+                                        <Command.Item className={CommandItemClasses} onSelect={() => handleSaveToWorkspace(workspace)}
+                                            key={workspace.id}>
+                                                {workspace.name}<Save className='w-4 h-4 ml-auto' />
+                                        </Command.Item>
+                                    ))}
+                                </>
+                            )}   
+                            {workspaces.map((workspace) => (
                                 <span key={workspace.id} className={cn('hidden underline ml-3', {'inline': workspace.id === selectedChat.workspaceId})}>{workspace.name}</span>
                             ))}
-                        </div>
+                        </Command.Group>
                     </>
                 )}
 
@@ -201,6 +301,21 @@ const Cmkd = ({chats, workspaces}: Props) => {
                         ))}
                     </>
                 )}
+                {page === 'newWorkspace' && (
+                    <Command.Group className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-max'>
+                        <input
+                        ref={workspaceNameInputRef}
+                        onChange={(e) => setWorkspaceName(e.target.value)}
+                        value={workspaceName}
+                        type="text"
+                        name="workspaceName"
+                        id="workspaceName"
+                        className="rounded-md border-0 py-1.5 text-white bg-transparent shadow-sm ring-1 ring-inset ring-transparent placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-transparent sm:text-sm sm:leading-6"
+                        placeholder="enter a name"
+                        />
+                        <Button className='h-[30px]' onClick={() => createWorkspace()}><Save className='w-4 h-4 mr-2' /> Save</Button>
+                    </Command.Group>
+                )}                     
             </Command.List>
       </Command.Dialog>
         </>
