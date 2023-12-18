9b5d702075771cf108fb640857ec90eb05ab4caa
Adhurim Gervalla <adhurim@gervalla.ch>
2023-12-18 09:37:08 +0100

    add delete user clerk event to api endpoint

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 import { users } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
 export const runtime = 'edge';
 
@@ -9,17 +10,33 @@ export async function POST(req: Request) {
     
     try {
         const payload: WebhookEvent = await req.json();
-        if (payload.type !== "user.created") return NextResponse.json({ message: "not a user created event" }, { status: 400 });
+        if (payload.type !== "user.created" && payload.type !== "user.deleted") return NextResponse.json({ message: "not a correct event to handle" }, { status: 400 });
         if (!payload.data.id) return Response.json({ message: "user could no be created because of missing id" }, { status: 400 });
-        const user_id = await db
-            .insert(users)
-            .values({
-                userId: payload.data.id,
-                email: payload.data.email_addresses[0].email_address,
-            }).returning({
-                insertedId: users.userId
-            });
-            return NextResponse.json({user_id: user_id[0].insertedId}, {status: 200});
+        
+        if (payload.type === "user.created") {
+            const user_id = await db
+                .insert(users)
+                .values({
+                    userId: payload.data.id,
+                    email: payload.data.email_addresses[0].email_address,
+                }).returning({
+                    insertedId: users.userId
+                });
+                return NextResponse.json({user_id: user_id[0].insertedId}, {status: 200});
+        }
+
+        if (payload.type === "user.deleted") {
+            const user_id = await db
+                .delete(users)
+                .where(eq(
+                    users.userId,
+                    payload.data.id,
+                )).returning({
+                    deletedId: users.userId
+                });
+                return NextResponse.json({user_id: user_id[0].deletedId}, {status: 200});
+        }
+        
         } catch (error) {
             return NextResponse.json({
                 error,
