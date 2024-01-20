00ab2855c0ce1f0c75d7fa68386498637b53400a
Adhurim Gervalla <adhurim@gervalla.ch>
2024-01-20 21:16:20 +0100

    fix search res

--- components/Cmdk/Chats/ChatsList.tsx
+++ components/Cmdk/Chats/ChatsList.tsx
@@ -14,7 +14,7 @@ const ChatsList = ({chats, currentChatId, handleDetailView}: Props) => {
   return (
     chats.map((chat) => (
         <ListItem onSelect={() => handleDetailView(chat)} cnObjects={[{'bg-green-700 text-white': isCurrentChat(chat.id, currentChatId)}]} key={chat.id}>
-            {chat.bookmarked && <Bookmark className='w-4 h-4' color={isCurrentChat(chat.id, currentChatId) ? 'white': 'green'} />} {chat.title && trimChatTitle(chat.title)}
+            {chat.bookmarked && <Bookmark className='w-4 h-4' color={isCurrentChat(chat.id, currentChatId) ? 'white': 'green'} />} {chat.title && chat.title}
         </ListItem>
     ))
   )
