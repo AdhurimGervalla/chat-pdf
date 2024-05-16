991c9bde4c34d5e4c818814a94b4440ead87ad76
Adhurim Gervalla <adhurim@gervalla.ch>
2024-05-16 11:23:02 +0200

    update model to 4o

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -69,7 +69,7 @@ export async function POST(req: NextRequest) {
         }
 
         // const model = isPro || userId === 'user_2Y05V0SAZMX7yxWRHFCiwMxCGog' ? 'gpt-4-1106-preview' : 'gpt-3.5-turbo';
-        const model = 'gpt-4';
+        const model = 'gpt-4o';
         const response = await openai.createChatCompletion({
             model,
             messages: [
